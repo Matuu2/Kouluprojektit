87f9b72fed3d552049aa74b560adbfcddf52db1f
Matuu2 <matuu2@hotmail.fi>
2017-03-21 11:19:12 +0200

    Update main.cpp

--- harjoitustyo-04/main.cpp
+++ harjoitustyo-04/main.cpp
@@ -1,6 +1,5 @@
 // TIE-02200 Ohjelmoinnin perusteet Harjoitustyö 4
-// Matias Ala-Sunila 246417
-// matias.ala-sunila@student.tut.fi
+// Matias Ala-Sunila 
 
 #include "painoindeksilaskin.hh"
 #include <QApplication>
